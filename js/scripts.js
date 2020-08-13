3e5988138865d7ce7e2ed7b9c304dc0ed32a665d
root <root@react.local>
2020-08-13 22:54:05 +1000

    add upload and download data from/to json-sever

--- js/scripts.js
+++ js/scripts.js
@@ -180,34 +180,23 @@ class MenuCard {
         this.parent.append(element);
     }
 }
-    new MenuCard(
-        "img/tabs/vegy.jpg",
-        "vegy",
-        "Меню 'Фитнес'",
-        "Меню 'Фитнес' - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!",
-        9,
-        ".menu .container"
-    ).render();
 
-    new MenuCard(
-        "img/tabs/elite.jpg",
-        "elite",
-        "Меню 'Премиум'",
-        "В меню 'Премиум' мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!",
-        19,
-        ".menu .container",
-        "menu__item"
-    ).render();
+const getResource = async (url) => {
+    const result = await fetch(url);
 
-    new MenuCard(
-        "img/tabs/post.jpg",
-        "post",
-        "Меню 'Постное'",
-        "Меню 'Постное' - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.",
-        14,
-        ".menu .container",
-        "menu__item"
-    ).render();
+    if(!result.ok) {
+        throw new Error(`Could not fetch ${url}, status: ${result.status}`);
+    }
+
+    return await result.json();
+}
+
+    getResource('http://food.local:3000/menu', )
+        .then(data => {
+            data.forEach(({img, altimg, title, descr, price}) => {
+                new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+            })
+        })
 
     // Forms
 
@@ -222,11 +211,22 @@ class MenuCard {
 
 
     forms.forEach(item => {
-        postData(item);
+        bindPostData(item);
     });
     
+    const postData = async (url, data) => {
+        const result = await fetch(url, {
+            method: "POST",
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: data
+        });
 
-    function postData(form) {
+        return await result.json();
+    }
+
+    function bindPostData(form) {
         form.addEventListener('submit', (event) => {
             event.preventDefault();
         
@@ -237,36 +237,23 @@ class MenuCard {
                 margin: 0 auto;
             `;
             form.insertAdjacentElement('afterend', statusMessage);
-
-                        
-
             
             const formData = new FormData(form);
 
-            const object = {};
-            formData.forEach(function(value, key){
-                object[key] = value;
-            });
+            const json = JSON.stringify(Object.fromEntries(formData.entries()));
 
-            fetch('server.php', {
-                method: "POST",
-                headers: {
-                    'Content-type': 'application/json'
-                },
-                body: JSON.stringify(object)
-            })
-            .then(data => data.text())
-            .then(data => {
-                console.log(data);
-                showThanksModal(message.success);
-                statusMessage.remove();
-            })
-            .catch(() => {
-                showThanksModal(message.failure);
-            })
-            .finally(() => {
-                form.reset(); 
-            })
+            postData('http://food.local:3000/requests', json)
+                .then(data => {
+                    console.log(data);
+                    showThanksModal(message.success);
+                    statusMessage.remove();
+                })
+                .catch(() => {
+                    showThanksModal(message.failure);
+                })
+                .finally(() => {
+                    form.reset(); 
+                })
         })
     }
 
@@ -293,6 +280,6 @@ class MenuCard {
         }, 4000);
     }
 
-    
+   
 
 });
