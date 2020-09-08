e1584500eb287ab10bdf186239adfcc0d3ed980c
root <root@react.local>
2020-09-09 00:01:30 +1000

    100%

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,34 @@
-"use strict";
+'use strict';
 import calc from './modules/calc';
-    import cards from './modules/cards';
-    import forms from './modules/forms';
-    import modal from './modules/modal';
-    import sliders from './modules/sliders';
-    import tabs from './modules/tabs';
-    import timer from './modules/timer';
-    import {openModal} from './modules/modal';
+import cards from './modules/cards';
+import forms from './modules/forms';
+import modal from './modules/modal';
+import sliders from './modules/sliders';
+import tabs from './modules/tabs';
+import timer from './modules/timer';
+import {
+	openModal
+} from './modules/modal';
 
 window.addEventListener('DOMContentLoaded', () => {
-    
-   const modalTimerId = setTimeout(() => openModal('.modal', modalTimerId), 600000);
 
-   calc();
-   cards();
-   forms('form', modalTimerId);
-   modal('[data-modal]', '.modal', modalTimerId);
-   sliders({
-    container: '.offer__slider',
-    slide: '.offer__slide',
-    prevArrow: '.offer__slider-prev',
-    nextArrow: '.offer__slider-next',
-    totalCount: '#total',
-    currentCount: '#current',
-    wrapper: '.offer__slider-wrapper',
-    inner: '.offer__slider-inner' 
-   });
-   tabs('.tabheader__item', '.tabcontent', '.tabheader__items', 'tabheader__item_active');
-   timer('.timer', "2020-09-23");  
- 
+	const modalTimerId = setTimeout(() => openModal('.modal', modalTimerId), 600000);
+
+	calc();
+	cards();
+	forms('form', modalTimerId);
+	modal('[data-modal]', '.modal', modalTimerId);
+	sliders({
+		container: '.offer__slider',
+		slide: '.offer__slide',
+		prevArrow: '.offer__slider-prev',
+		nextArrow: '.offer__slider-next',
+		totalCounter: '#total',
+		currentCounter: '#current',
+		wrapper: '.offer__slider-wrapper',
+		inner: '.offer__slider-inner'
+	});
+	tabs('.tabheader__item', '.tabcontent', '.tabheader__items', 'tabheader__item_active');
+	timer('.timer', '2020-09-23');
+
 });
